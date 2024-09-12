eed891fbd4ab9462c55df10feb448a01d7487170
unco <1054093062@qq.com>
2024-09-13 03:44:40 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,6 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
 use syn::{self, Data};
-pub use mask_system_lib::MaskSystem;
-pub use mask_system_lib::CheckMaskSystem;
-pub use mask_system_lib::MaskSystemContent;
-pub use mask_system_lib::Content;
-pub use mask_system_lib::Tag_1_2;
-pub use mask_system_lib::Tag_2_4;
-pub use mask_system_lib::Tag_3_8;
-pub use mask_system_lib::Tag_4_16;
-pub use mask_system_lib::Tag_5_32;
-pub use mask_system_lib::Tag_6_64;
-pub use mask_system_lib::op;
-pub use mask_system_lib::Unsigned;
-pub use mask_system_lib::validate_mask;
-pub use mask_system_lib::validate_max_1;
-pub use mask_system_lib::F;
-pub use mask_system_lib::Flat;
-pub use mask_system_lib::PickTuple;
 
 #[proc_macro_derive(MaskSys)]
 pub fn MaskSys(input: TokenStream) -> TokenStream {
